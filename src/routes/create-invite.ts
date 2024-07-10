87efd18979fe3a2f1a450d9cf6f402f576f50485
Norberto Boa <nboa.26@gmail.com>
2024-07-10 23:25:52 +0200

    ADD create invite route

---
+++ src/routes/create-invite.ts
@@ -0,0 +1,68 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { prisma } from "../lib/prisma";
+import { createLinksBodySchema, createLinksParamsSchema } from "../validators/links/createLinksSchema";
+import { tripIdAsParamsSchema } from "../validators/global";
+import { createInviteSchema } from "../validators/participant/createInviteSchema";
+import { getMailClient } from "../lib/mail";
+import nodemailer from 'nodemailer';
+import { formatDate } from "../lib/formatDate";
+
+export async function createInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/invites', {
+    schema: {
+      body: createInviteSchema,
+      params: tripIdAsParamsSchema
+    }
+  }, async (req) => {
+    const { tripId } = req.params;
+    const { email } = req.body;
+
+    const trip = await prisma.trip.findUnique({
+      where: {
+        id: tripId
+      }
+    });
+
+    if (!trip) {
+      throw new Error("Viagem nao encontrada!");
+    }
+
+    const participant = await prisma.participant.create({
+      data: {
+        email,
+        trip_id: tripId,
+      }
+    })
+
+    const formattedStartDate = formatDate(trip.starts_at)
+    const formattedEndDate = formatDate(trip.ends_at);
+
+    const mail = await getMailClient();
+    const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`
+    const message = await mail.sendMail({
+      from: {
+        name: "Equip Plann.er",
+        address: "planner@anytech.co.mz"
+      },
+      to: participant.email,
+      subject: `Confirme sua viagem para ${trip.destination} em ${formattedStartDate}`,
+      html: `
+            <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+              <p>Você foi convidado(a) para participar de uma viagem para <strong>${trip.destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+              <p></p>
+              <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+              <p></p>
+              <p>
+                <a href="${confirmationLink}">Confirmar viagem</a>
+              </p>
+              <p></p>
+              <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+            </div>
+            `.trim()
+    });
+    console.log(nodemailer.getTestMessageUrl(message));
+
+    return { participant };
+  });
+}

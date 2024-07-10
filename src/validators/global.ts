802ee3c8add04db07fa5ba5f4c4e1a979456cb44
Norberto Boa <nboa.26@gmail.com>
2024-07-11 00:42:16 +0200

    ADD get participant route

--- src/validators/global.ts
+++ src/validators/global.ts
@@ -3,3 +3,6 @@ import z from "zod";
 export const tripIdAsParamsSchema = z.object({
   tripId: z.string().uuid(),
 });
+export const participantIdAsParamsSchema = z.object({
+  participantId: z.string().uuid(),
+});

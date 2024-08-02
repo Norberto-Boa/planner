acde98af950c9115ea006c6296574e579e181ba8
Norberto Boa <nboa.26@gmail.com>
2024-08-03 00:18:58 +0200

    ADD: Create trip service

--- src/services/tripService.ts
+++ src/services/tripService.ts
@@ -1,6 +1,38 @@
 import { Trip } from "@prisma/client";
 import { prisma } from "../lib/prisma";
+import z from "zod";
+import { createTripSchema } from "../validators/trips/createTripSchema";
 
+type createTripDTO = z.infer<typeof createTripSchema>;
+
+// Create Trip
+export async function createTrip({ destination, emails_to_invite, ends_at, owner_email, owner_name, starts_at }: createTripDTO) {
+  return await prisma.trip.create({
+    data: {
+      destination,
+      starts_at,
+      ends_at,
+      participants: {
+        createMany: {
+          data: [
+            {
+              email: owner_email,
+              name: owner_name,
+              is_owner: true,
+              is_confirmed: true,
+            },
+            ...emails_to_invite.map(email => {
+              return { email }
+            })
+          ]
+        }
+      }
+    }
+  })
+}
+
+
+// Get Trip by Trip Id
 export async function getTripByIdService(id: string): Promise<Trip | null> {
   return await prisma.trip.findUnique({
     where: {
